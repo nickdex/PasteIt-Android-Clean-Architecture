1678c07a12b22e6781937f3e1838018b76bc01bc
Nikhil Warke <nikwarke@gmail.com>
2016-10-23 11:21:30 +0530

    Add ClipBoardListener class

---
+++ app/src/main/java/io/github/nickdex/pasteit/ClipBoardListener.java
@@ -0,0 +1,11 @@
+package io.github.nickdex.pasteit;
+
+/**
+ * Listens to clipboard for new items and sends it to service
+ *
+ * @author Nikhil Warke
+ * @since 23/10/16.
+ */
+
+public class ClipBoardListener {
+}
